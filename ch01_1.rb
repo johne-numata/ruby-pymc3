815647c8be0a1123a96cb499c66b1daf3b39c4c2
johne-numata <johne.numata@gmail.com>
2017-12-03 09:45:34 +0900

    試用ファイル

---
+++ ch01_1.rb
@@ -0,0 +1,120 @@
+require 'pycall/import'
+include PyCall::Import
+
+pyimport  'numpy', as: :np
+plt = pyfrom  'matplotlib', import: :pyplot
+stats = pyfrom 'scipy', import: :stats
+a = np.arange(16)
+poi = stats.poisson
+lambda_ = [1.5, 4.25]
+colours = ["#348ABD", "#A60628"]
+
+plt.bar(a, poi.pmf(a, lambda_[0]), color=colours[0],
+        label="$\lambda = %.1f$" % lambda_[0], alpha=0.60,
+        edgecolor=colours[0], lw="3")
+
+plt.bar(a, poi.pmf(a, lambda_[1]), color=colours[1],
+        label="$\lambda = %.1f$" % lambda_[1], alpha=0.60,
+        edgecolor=colours[1], lw="3")
+
+plt.xticks(a + 0.4, a)
+plt.legend()
+plt.ylabel("probability of $k$")
+plt.xlabel("$k$")
+plt.title("Probability mass function of a Poisson random variable; differing $\lambda$ values")
+#plt.show()
+
+=begin
+a = np.linspace(0, 4, 100)
+expo = stats.expon
+lambda_ = [0.5, 1]
+
+for l, c in zip(lambda_, colours):
+    plt.plot(a, expo.pdf(a, scale=1. / l), lw=3,
+             color=c, label="$\lambda = %.1f$" % l)
+    plt.fill_between(a, expo.pdf(a, scale=1. / l), color=c, alpha=.33)
+
+plt.legend()
+plt.ylabel("PDF at $z$")
+plt.xlabel("$z$")
+plt.ylim(0, 1.2)
+plt.title("Probability density function of an Exponential random variable;\
+ differing $\lambda$");
+
+#plt.show()
+
+
+count_data = np.loadtxt("data/txtdata.csv")
+n_count_data = len(count_data)
+plt.bar(np.arange(n_count_data), count_data, color="#348ABD")
+plt.xlabel("Time (days)")
+plt.ylabel("count of text-msgs received")
+plt.title("Did the user's texting habits change over time?")
+plt.xlim(0, n_count_data);
+
+#plt.show()
+
+import pymc as pm
+
+alpha = 1.0 / count_data.mean()  # Recall count_data is the
+                               # variable that holds our txt counts
+lambda_1 = pm.Exponential("lambda_1", alpha)
+lambda_2 = pm.Exponential("lambda_2", alpha)
+
+tau = pm.DiscreteUniform("tau", lower=0, upper=n_count_data)
+
+print("Random output:", tau.random(), tau.random(), tau.random())
+
+@pm.deterministic
+def lambda_(tau=tau, lambda_1=lambda_1, lambda_2=lambda_2):
+    out = np.zeros(n_count_data)
+    out[:tau] = lambda_1  # lambda before tau is lambda1
+    out[tau:] = lambda_2  # lambda after (and including) tau is lambda2
+    return out
+
+observation = pm.Poisson("obs", lambda_, value=count_data, observed=True)
+
+model = pm.Model([observation, lambda_1, lambda_2, tau])
+
+mcmc = pm.MCMC(model)
+mcmc.sample(40000, 10000, 1)
+
+lambda_1_samples = mcmc.trace('lambda_1')[:]
+lambda_2_samples = mcmc.trace('lambda_2')[:]
+tau_samples = mcmc.trace('tau')[:]
+
+
+ax = plt.subplot(311)
+ax.set_autoscaley_on(False)
+
+plt.hist(lambda_1_samples, histtype='stepfilled', bins=30, alpha=0.85,
+         label="posterior of $\lambda_1$", color="#A60628", normed=True)
+plt.legend(loc="upper left")
+plt.title(r"""Posterior distributions of the variables
+    $\lambda_1,\;\lambda_2,\;\tau$""")
+plt.xlim([15, 30])
+plt.xlabel("$\lambda_1$ value")
+
+ax = plt.subplot(312)
+ax.set_autoscaley_on(False)
+plt.hist(lambda_2_samples, histtype='stepfilled', bins=30, alpha=0.85,
+         label="posterior of $\lambda_2$", color="#7A68A6", normed=True)
+plt.legend(loc="upper left")
+plt.xlim([15, 30])
+plt.xlabel("$\lambda_2$ value")
+
+plt.subplot(313)
+w = 1.0 / tau_samples.shape[0] * np.ones_like(tau_samples)
+plt.hist(tau_samples, bins=n_count_data, alpha=1,
+         label=r"posterior of $\tau$",
+         color="#467821", weights=w, rwidth=2.)
+plt.xticks(np.arange(n_count_data))
+
+plt.legend(loc="upper left")
+plt.ylim([0, .75])
+plt.xlim([35, len(count_data) - 20])
+plt.xlabel(r"$\tau$ (in days)")
+plt.ylabel("probability");
+
+plt.show()
+=end
